e72c8199d8238f3100340ce3f9e022548b2c5fed
1roo <93hanaru@gmail.com>
2025-05-29 23:05:02 +0900

    initial commit

---
+++ src/main/java/com/hanarae/availo/AvailoApplication.java
@@ -0,0 +1,13 @@
+package com.hanarae.availo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvailoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvailoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanarae/availo/AvailoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanarae.availo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvailoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
